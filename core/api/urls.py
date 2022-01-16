970db052115018670ef250d31b8c2a3a526dbe4f
mowbish <mobin.clash.ma@gmail.com>
2022-01-16 20:25:56 +0330

    Changes

--- core/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('accounts.api.urls')),
-    path('', include('products.api.urls')),
-    path('', include('orders.api.urls')),
-]
