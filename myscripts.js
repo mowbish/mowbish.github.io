6b64419e8ffdb2c2f5a743507db58d1f6c3e03e2
Mobin <mobin.clash.ma@gmail.com>
2022-01-17 13:14:56 +0330

    Time was Updated!

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
+var countDownDate = new Date("February 16, 2022 20:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function () {
